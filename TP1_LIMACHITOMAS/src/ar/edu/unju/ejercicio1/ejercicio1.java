214e032972b045d5c664b14f278f10aaa6ded916
TOMIS <TOMIS@DESKTOP-BSTI8S1>
2024-04-05 19:59:44 -0300

    ejercicio 1

---
+++ TP1_LIMACHITOMAS/src/ar/edu/unju/ejercicio1/ejercicio1.java
@@ -0,0 +1,20 @@
+package ar.edu.unju.ejercicio1;
+
+public class ejercicio1 {
+
+	public static void main(String[] args) {
+		
+		
+		// TODO Auto-generated method stub
+		
+		int a = 1;
+		int b = 2;
+		int c= 3;
+		int d = 4;
+		int e = 5;
+		int f = (a+b+c+d+e)/5;
+		
+				System.out.println("promedio" + f);
+	}
+
+}
