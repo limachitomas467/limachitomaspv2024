1d24577e1e685926b42d018305a812c7f646dbaf
TOMIS <TOMIS@DESKTOP-BSTI8S1>
2024-04-05 18:55:22 -0300

    prueba primero

---
+++ proyecto1/src/ar/edu/unju/pv/ejemplo1/Prueba.java
@@ -0,0 +1,42 @@
+package ar.edu.unju.pv.ejemplo1;
+import java.util.Scanner;
+
+public class Prueba {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int anio=2024;
+		String saludo="hola";
+     System.out.println(saludo + anio);
+     
+     if  (anio<=2024)
+     {
+    	 System.out.println("año correcto");
+    	 
+     }
+     else
+     {System.out.println("año incorrecto");
+     }
+      int tamanio =saludo.length();
+      
+     for (int i=0; i<tamanio;i++)
+     {
+     System.out.println(saludo.charAt(i));
+     }
+     int indice = 0;
+     while (indice < tamanio)
+     {
+     System.out.println(saludo.charAt(indice));
+     indice++;
+     }
+     
+     Scanner teclado = new Scanner(System.in);
+     System.out.println("ingrese nombre");
+     String nombre = teclado.next();
+     System.out.println("hola"+ nombre);
+     
+    }
+
+	
+
+}
